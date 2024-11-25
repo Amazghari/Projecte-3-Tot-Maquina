632be5514a9bbe6b42e86f16db8f8d9a8762796a
joelpages02 <jpages@cendrassos.net>
2024-11-25 19:50:00 +0100

    Poner las routes de quasi todas las views en el index public

--- public/index.php
+++ public/index.php
@@ -33,7 +33,14 @@ $app = new \Emeset\Emeset($contenidor);
 $app->middleware([\App\Middleware\App::class, "execute"]);
 
 $app->route("/inicio", [\App\Controllers\inicio::class, "inicio"]);
-
+// $app->route("/inventario", [\App\Controllers\inventario::class, "inventario"]);
+// $app->route("/incidencias", [\App\Controllers\incidencias::class, "incidencias"]);
+// $app->route("/mantenimiento_preventivo", [\App\Controllers\mantenimiento_preventivo::class, "mantenimiento_preventivo"]);
+// $app->route("/estadisticas", [\App\Controllers\estadisticas::class, "estadisticas"]);
+// $app->route("/dashboard", [\App\Controllers\dashboard::class, "dashboard"]);
+// $app->route("/perfil", [\App\Controllers\perfil::class, "perfil"]);  
+// $app->route("/mantenimiento", [\App\Controllers\mantenimiento::class, "mantenimiento"]); 
+  
 $app->route("login", "ctrlLogin");
 $app->route("validar-login", "ctrlValidarLogin");
 $app->route("privat", [\App\Controllers\Privat::class, "privat"], ["auth"]);
