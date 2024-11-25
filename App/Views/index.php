d2ed2e4535c3bedeacf7b9d383974b8aa1967974
joelpages02 <jpages@cendrassos.net>
2024-11-25 17:08:44 +0100

    View index funcional

--- App/Views/index.php
+++ App/Views/index.php
@@ -5,13 +5,16 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+  <link href="main.css" rel="stylesheet">
+  
+  
   <title>Acceso al sistema</title>
 </head>
 
-<body class="bg-gray-200 flex items-center justify-center h-screen">
+<body class="bg-custom-light-gray flex items-center justify-center h-screen">
   <div class="bg-white rounded-lg shadow-lg p-6 w-96">
     <h2 class="text-center text-xl font-bold mb-4">Acceso al sistema</h2>
+    <img src="/uploads/img/logopng.png" alt="Logo" class="w-16 mx-auto mb-4">
     <form>
       <div class="mb-4">
         <label class="block text-gray-700" for="usuario">Usuario</label>
@@ -21,10 +24,10 @@
         <label class="block text-gray-700" for="contrasena">Contraseña</label>
         <input class="border border-gray-300 rounded w-full p-2" type="password" id="contrasena" placeholder="Ingrese su contraseña">
       </div>
-      <button class="bg-blue-500 text-white rounded w-full p-2 hover:bg-blue-600" type="submit">Inicio Sesión</button>
+      <button class="bg-custom-blue text-white rounded w-full p-2 hover:bg-custom-blue" type="submit">Inicio Sesión</button>
     </form>
     <p class="text-center text-gray-600 mt-4">
-      <a href="#" class="text-blue-500 hover:underline">¿Olvidó su contraseña?</a>
+      <a href="#" class="text-custom-blue hover:underline">¿Olvidó su contraseña?</a>
     </p>
   </div>
 </body>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,13 @@ module.exports = {
     "./App/**/*.php",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // 'custom-blue': '#003366',
+        // 'custom-light-gray': '#E0E0E0',
+        // 'custom-gray': '#757575',
+      },
+    },
   },
   plugins: [],
 }
