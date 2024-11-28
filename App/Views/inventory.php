20fea89630f0d540ad50f8a38a2511acf853873a
joelpages02 <jpages@cendrassos.net>
2024-11-28 19:08:45 +0100

    Modificar modal añadir maquina

--- App/Views/inventory.php
+++ App/Views/inventory.php
@@ -28,32 +28,29 @@
                             <th class="px-6 py-3 text-left text-sm font-semibold">ID</th>
                             <th class="px-6 py-3 text-left text-sm font-semibold">Nombre</th>
                             <th class="px-6 py-3 text-left text-sm font-semibold">Nºserie</th>
-                            <th class="px-6 py-3 text-left text-sm font-semibold">Estado</th>
                             <th class="px-6 py-3 text-left text-sm font-semibold">ID Encargado</th>
                             <th class="px-6 py-3 text-left text-sm font-semibold">Nombre Encargado</th>
                             <th class="px-6 py-3 text-left text-sm font-semibold">Opciones</th>
                         </tr>
                     </thead>
+                    <?php foreach ($machines as $machine) { ?>
                     <tbody class="divide-y divide-gray-200">
                         <tr class="hover:bg-gray-50">
-                            <td class="px-6 py-4 text-sm text-gray-900">#MAQ-001</td>
+                            <td class="px-6 py-4 text-sm text-gray-900">#MAQ-<?= $machine["id"] ?></td>
                             <td class="px-6 py-4 text-sm text-gray-900">
-                                <p class="truncate max-w-[200px]">Fresadora</p>
+                                <p class="truncate max-w-[200px]"><?= $machine["name"] ?></p>
                             </td>
-                            <td class="px-6 py-4">1231232132</td>
-                            <td class="px-6 py-4">
-                                <span class="px-3 py-1 text-xs font-medium rounded-full bg-green-100 text-green-800">Operativa</span>
-                            </td>
+                            <td class="px-6 py-4"><?= $machine["serial_num"] ?></td>
                             <td class="px-6 py-4 text-sm text-gray-900">USR-123</td>
                             <td class="px-6 py-4 text-sm text-gray-900">Miguelito</td>
                             <td class="px-6 py-4 text-sm">
                                 <div class="flex space-x-3">
-                                    <button class="text-blue-600 hover:text-blue-800">
+                                    <button class="text-blue-600 hover:text-blue-800" data-id="<?= $machine["id"] ?>" data-bs-toggle="modal" data-bs-target="#modal-editar">
                                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
                                         </svg>
                                     </button>
-                                    <button class="text-red-600 hover:text-red-800">
+                                    <button class="text-red-600 hover:text-red-800" data-id="<?= $machine["id"] ?>">
                                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
                                         </svg>
@@ -62,6 +59,7 @@
                             </td>
                         </tr>
                     </tbody>
+                    <?php } ?> 
                 </table>
             </div>
         </div>
@@ -86,27 +84,27 @@
                 <form class="space-y-4 mt-4" action="/inventario/addMachine" method="post" enctype="multipart/form-data">
                     <div>
                         <label class="block text-sm font-medium text-gray-700">Nombre</label>
-                        <input type="text" name="machineName" id="machineName" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50">
+                        <input type="text" name="machineName" id="machineName" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50 bg-custom-light-gray">
                     </div>
                     <div>
                         <label class="block text-sm font-medium text-gray-700">Nº Serie</label>
-                        <input type="text" name="serialNumber" id="serialNumber" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50">
+                        <input type="text" name="serialNumber" id="serialNumber" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50 bg-custom-light-gray">
                     </div>
                     <div>
                         <label class="block text-sm font-medium text-gray-700">Modelo</label>
-                        <input type="text" name="model" id="model" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50">
+                        <input type="text" name="model" id="model" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50 bg-custom-light-gray">
                     </div>
                     <div>
                         <label class="block text-sm font-medium text-gray-700">Fabricante</label>
-                        <input type="text" name="manufacturer" id="manufacturer" class="class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50">
+                        <input type="text" name="manufacturer" id="manufacturer" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50 bg-custom-light-gray">
                     </div>
                     <div>
                         <label class="block text-sm font-medium text-gray-700">Fecha instalación</label>
-                        <input type="date" name="installationDate" id="installationDate" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50">
+                        <input type="date" name="installationDate" id="installationDate" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50 bg-custom-light-gray">
                     </div>
                     <div>
                         <label class="block text-sm font-medium text-gray-700">Localización</label>
-                        <input type="text" name="location" id="location" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50">
+                        <input type="text" name="location" id="location" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50 bg-custom-light-gray">
                     </div>
                     <div>
                         <label class="block text-sm font-medium text-gray-700">Imagen</label>
@@ -127,6 +125,66 @@
             </div>
         </div>
 
+        <!-- Modal para editar -->
+         <?php foreach ($machines as $machine) { ?>
+        <div class="hidden fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" 
+             id="modal-editar"
+             role="dialog"
+             aria-modal="true">
+            <div class="modal-content p-6">
+                <div class="flex justify-between items-center pb-3 border-b">
+                    <h3 class="text-xl font-semibold text-gray-900">Editar Maquina</h3>
+                    <label for="modal-toggle" class="cursor-pointer text-gray-600 hover:text-gray-800">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+                        </svg>
+                    </label>
+                </div>
+                <form class="space-y-4 mt-4" action="/inventario/updateMachine" method="post" enctype="multipart/form-data">
+                    <input type="hidden" name="id" value="<?= $machine["id"] ?>">
+                    <div>
+                        <label class="block text-sm font-medium text-gray-700">Nombre</label>
+                        <input type="text" name="name" id="name" value="<?= $machine["name"] ?>" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50">
+                    </div>
+                    <div>
+                        <label class="block text-sm font-medium text-gray-700">Nº Serie</label>
+                        <input type="text" name="serial_num" id="serial_num" value="<?= $machine["serial_num"] ?>" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50">
+                    </div>
+                    <div>
+                        <label class="block text-sm font-medium text-gray-700">Modelo</label>
+                        <input type="text" name="model" id="model" value="<?= $machine["model"] ?>" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50">
+                    </div>
+                    <div>
+                        <label class="block text-sm font-medium text-gray-700">Fabricante</label>
+                        <input type="text" name="manufacturer" id="manufacturer" value="<?= $machine["manufacturer"] ?>" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50">
+                    </div>
+                    <div>
+                        <label class="block text-sm font-medium text-gray-700">Fecha instalación</label>
+                        <input type="date" name="installation_date" id="installation_date" value="<?= $machine["installation_date"] ?>" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50">
+                    </div>
+                    <div>
+                        <label class="block text-sm font-medium text-gray-700">Localización</label>
+                        <input type="text" name="location" id="location" value="<?= $machine["location"] ?>" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50">
+                    </div>
+                    <div>
+                        <label class="block text-sm font-medium text-gray-700">Imagen</label>
+                        <input type="file" name="image" id="image" accept="image/*" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-custom-blue focus:ring focus:ring-custom-blue focus:ring-opacity-50">
+                    </div> 
+                    
+                    <div class="flex justify-end space-x-3 mt-6 pt-4 border-t">
+                        <label for="modal-toggle"  
+                               class="px-4 py-2 bg-gray-100 text-gray-700 rounded-md hover:bg-gray-200 cursor-pointer">
+                            Cancelar  
+                        </label>
+                        <button type="submit" 
+                                class="px-4 py-2 bg-custom-blue text-white rounded-md hover:bg-blue-800 transition-colors">
+                            Guardar
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <?php } ?>
 </body>
 <!-- Footer -->
 <?php include 'Layouts/footer.php'; ?>
