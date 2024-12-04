13aa33f286ef4aa9fb4c8c853372694e9ea49cb1
joelpages02 <jpages@cendrassos.net>
2024-12-04 18:55:36 +0100

    Update de las credenciales + foto funcional

--- App/Controllers/profileController.php
+++ App/Controllers/profileController.php
@@ -21,28 +21,29 @@ class profileController {
         $directory="/uploads/users/";
 
         $currentuser = $_SESSION['user'];
-
         $id = $request->get(INPUT_POST, "iduser");
         $name = $request->get(INPUT_POST, "name");
         $surname = $request->get(INPUT_POST, "surname");
         $username = $request->get(INPUT_POST, "username");
         $role = $request->get(INPUT_POST, "role");
         $image=$request->get("FILES","image");
-        $image_url = $currentuser['img'];
 
-        if (isset($image)) {
+        $image_url = $currentuser['img'];
+        //dd($image['name'],$image_url);  
+
+        if (($image['name']!="")) {
             $image_url = $directory . $image["name"];
-            move_uploaded_file($image["tmp_name"], "uploads/users//" . $image["name"]);
+            move_uploaded_file($image["tmp_name"], "uploads/users/" . $image["name"]);
         }
         
         //dd($image_url);
 
         $profiles = $container->get("Users");
         $profiles->updateProfile($id,$name,$surname,$username,$role,$image_url);
-        $currentUser= $profiles->getUser($username);
+        $currentUser2= $profiles->getUser($username);
 
-        $response->setSession("user", $currentUser);
-        //dd($currentUser);
+        $response->setSession("user", $currentUser2);
+        //dd($_SESSION);
 
         $response->redirect("location: /perfil");
     
