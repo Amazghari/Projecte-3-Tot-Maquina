e87f6e1fce9131303a7c60fcfcf5284124b80c27
Nil Suñer Xarles <nsuner@cendrassos.net>
2024-12-09 19:24:47 +0100

    tabla mantenimientos admin

--- App/Controllers/adminmaintenanceController.php
+++ App/Controllers/adminmaintenanceController.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Controllers;
+use \Emeset\Contracts\Http\Request;
+use \Emeset\Contracts\Http\Response;
+use \Emeset\Contracts\Container;
 
 class adminmaintenanceController {
 
     public function adminmaintenanceController($request, $response, $container){
+        $machinesModel = $container->get("Machines");
+        $machines = $machinesModel->list();
+        $maintenanceModel = $container->get("Maintenances");
+        $maintenances = $maintenanceModel->list();
+        $response->set("machines", $machines);
+        $response->set("maintenances", $maintenances);
 
         $response->setTemplate("adminmaintenance.php");
 
